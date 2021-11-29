283fc7a2fd0e0c42690b13df9d363bce8ffe5ef7
Maxim.Durov <duroff.83@yandex.ru>
2021-11-29 17:10:48 +0700

    29.11

--- base/forms.py
+++ base/forms.py
@@ -4,6 +4,6 @@ from django import forms
 class LoginForm(forms.Form):
     login = forms.CharField(label='Имя пользователя',
                             max_length=100,
-                            widget=forms.TextInput(attrs={'placeholder': 'ЛОГИН'}))
+                            widget=forms.TextInput(attrs={'class': 'form-control form-control-sm', 'placeholder': 'ЛОГИН'}))
     password = forms.CharField(label='Пароль',
-                               widget=forms.PasswordInput(attrs={'placeholder': 'ПАРОЛЬ'}))
+                               widget=forms.PasswordInput(attrs={'class': 'form-control form-control-sm', 'placeholder': 'ПАРОЛЬ'}))
