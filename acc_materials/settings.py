7f3ea434af3c64f35297d9b06a9fd61859d18bba
Maxim Durov <maxim.durov@russianpost.ru>
2022-02-25 04:26:43 +0000

    settings

--- acc_materials/settings.py
+++ acc_materials/settings.py
@@ -64,18 +64,18 @@ WSGI_APPLICATION = 'acc_materials.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
+    #'default': {
+    #   'ENGINE': 'django.db.backends.sqlite3',
+    #   'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+     'default': {
+	'ENGINE': 'django.db.backends.postgresql_psycopg2',
+	'NAME': 'materials',
+	'USER': 'django_user',
+	'PASSWORD': '18atdhfkz',
+	'HOST': '127.0.0.1',
+	'PORT': '5432',
     }
-    # 'default': {
-	# 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-	# 'NAME': 'materials',
-	# 'USER': 'django_user',
-	# 'PASSWORD': '18atdhfkz',
-	# 'HOST': '127.0.0.1',
-	# 'PORT': '5432',
-    # }
 }
 
 
@@ -120,9 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = "/var/www/materials/static/"
-STATIC_ROOT = BASE_DIR / 'base/static/'
-#STATICFILES_DIRS = ['/var/www/materials/base/static']
+STATIC_ROOT = "/var/www/materials/static/"
+#STATIC_ROOT = BASE_DIR / 'base/static/'
+STATICFILES_DIRS = ['/var/www/materials/base/static']
 
 
 # Default primary key field type
